08a70171c4d6abee76d11382f98d2f7f5be3bcb6
Juan Ignacio Sánchez Sampayo <juan@192.168.1.3>
2023-09-30 01:27:26 -0300

    add new profile pic and improves some css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Roboto } from '@next/font/google'
 import Image from 'next/image'
 import styles from './page.module.css'
+
+const roboto = Roboto({ subsets: ['latin'], weight: "400" })
 
 function PageCard({ title, description }: { title: string, description: string }) {
 
@@ -20,37 +23,41 @@ function PageCard({ title, description }: { title: string, description: string }
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <span className={roboto.className}>
+      <main className={styles.main}>
 
-      <div className={styles.center}>
-        <div className={styles.description}>
-          <Image
-            src="./images/profile.jpg"
-            alt="Profile picture"
-            className={styles.profilePicture}
-            width={200}
-            height={200}
-            priority
-          />
+        <div className={styles.center}>
+          <div className={styles.description}>
+            <Image
+              src="./images/profile.jpg"
+              alt="Profile picture"
+              className={styles.profilePicture}
+              width={0}
+              height={0}
+              sizes="10vw"
+              style={{ width: '15rem', height: 'auto' }} // optional
+              priority
+            />
+          </div>
+          <h2 className={styles.profileName}>Juan Ignacio Sánchez Sampayo</h2>
+          <div className={styles.description}>
+
+            <p>I am a senior backend engineer, based in Montevideo, Uruguay, and have 9+ years
+              of experience working with Python/Django and similar technologies. I build systems,
+              robust RESTful APIs and web applications for many clients, focusing on the product,
+              finding solutions with autonomy, well balanced regarding delivery time and quality.
+              My strongest suit is in backend technologies but I`&apos;`m currently learning React/Next.js, this website is powered by such framework.</p>
+          </div>
         </div>
-        <h2 className={styles.profileName}>Juan Ignacio Sánchez Sampayo</h2>
-        <div className={styles.description}>
 
-          <p>I am a senior backend engineer, based in Montevideo, Uruguay, and have 9+ years
-            of experience working with Python/Django and similar technologies. I build systems,
-            robust RESTful APIs and web applications for many clients, focusing on the product,
-            finding solutions with autonomy, well balanced regarding delivery time and quality.
-            My strongest suit is in backend technologies but I`&apos;`m currently learning React/Next.js, this website is powered by such framework.</p>
+
+        <div className={styles.grid}>
+          <PageCard title="Contact Information" description="If you would like to do business, please reach out!" />
+          <PageCard title="Work Experience" description="If you would like to do business, please reach out!" />
+          <PageCard title="Education" description="If you would like to do business, please reach out!" />
+          <PageCard title="Personal Projects" description="If you would like to do business, please reach out!" />
         </div>
-      </div>
-
-
-      <div className={styles.grid}>
-        <PageCard title="Contact Information" description="If you would like to do business, please reach out!" />
-        <PageCard title="Work Experience" description="If you would like to do business, please reach out!" />
-        <PageCard title="Education" description="If you would like to do business, please reach out!" />
-        <PageCard title="Personal Projects" description="If you would like to do business, please reach out!" />
-      </div>
-    </main>
+      </main>
+    </span>
   )
 }
