6f27de88db955856c7ca93446e5077e04a3015f1
Juan Ignacio Sánchez Sampayo <juan@192.168.1.3>
2023-09-29 23:37:26 -0300

    refactor some components and adds PageCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,55 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+function PageCard({ title, description }: { title: string, description: string }) {
+
+  return (
+    <a
+      href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>
+        {title}
+      </h2>
+      <p>{description}</p>
+    </a>
+  )
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Jolines estoy editando este archivo:&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="images/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+      <div className={styles.center}>
+        <div className={styles.description}>
+          <Image
+            src="./images/profile.jpg"
+            alt="Profile picture"
+            className={styles.profilePicture}
+            width={200}
+            height={200}
+            priority
+          />
+        </div>
+        <h2 className={styles.profileName}>Juan Ignacio Sánchez Sampayo</h2>
+        <div className={styles.description}>
+
+          <p>I am a senior backend engineer, based in Montevideo, Uruguay, and have 9+ years
+            of experience working with Python/Django and similar technologies. I build systems,
+            robust RESTful APIs and web applications for many clients, focusing on the product,
+            finding solutions with autonomy, well balanced regarding delivery time and quality.
+            My strongest suit is in backend technologies but I`&apos;`m currently learning React/Next.js, this website is powered by such framework.</p>
         </div>
       </div>
 
-      <div className={styles.center}>
-
-        <Image
-          className={styles.logo}
-          src="images/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <PageCard title="Contact Information" description="If you would like to do business, please reach out!" />
+        <PageCard title="Work Experience" description="If you would like to do business, please reach out!" />
+        <PageCard title="Education" description="If you would like to do business, please reach out!" />
+        <PageCard title="Personal Projects" description="If you would like to do business, please reach out!" />
       </div>
     </main>
   )
