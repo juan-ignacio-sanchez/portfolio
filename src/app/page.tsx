44bb7c5ccdb181e1a01fa1b2081ce9563d562a96
Juan Ignacio Sánchez Sampayo <juan@192.168.1.14>
2023-10-12 12:56:34 -0300

    styles refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
+"use client"
 import { Roboto } from '@next/font/google'
+import confetti from 'canvas-confetti'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from './page.module.css'
+
 
 const roboto = Roboto({ subsets: ['latin'], weight: "400" })
 
-function PageCard({ title, description }: { title: string, description: string }) {
+function Profile({ displayProfile = true, toggleProfile }: { displayProfile: boolean, toggleProfile: any }) {
+  return (
+    <div style={{ display: displayProfile ? 'flex' : 'none' }} className={styles.profile}>
+      <Image
+        src="./images/profile.jpg"
+        alt="Profile picture"
+        className={styles.profilePicture}
+        width={0}
+        height={0}
+        priority
+        onClick={() => confetti()}
+      />
+      <h2 className={styles.profileName}>Juan Ignacio Sánchez Sampayo</h2>
+
+      <p className={styles.description}>I am a senior backend engineer, based in Montevideo, Uruguay, and have 9+ years
+        of experience working with Python/Django and similar technologies. I build systems,
+        robust RESTful APIs and web applications for many clients, focusing on the product,
+        finding solutions with autonomy, well balanced regarding delivery time and quality.
+        My strongest suit is in backend technologies but I am currently learning React/Next.js, this website is powered by such framework.</p>
+    </div>
+  )
+}
+
+function PageCard({ initialTitle, description, icon }: { initialTitle: string, description: string, icon: string }) {
+  const [title, setTitle] = useState(initialTitle);
 
   return (
     <a
@@ -13,49 +41,42 @@ function PageCard({ title, description }: { title: string, description: string }
       target="_blank"
       rel="noopener noreferrer"
     >
-      <h2>
-        {title}
-      </h2>
-      <p>{description}</p>
-    </a>
+      <Image
+        src={`./images/${icon}.svg`}
+        alt="Profile picture"
+        width={100}
+        height={100}
+        priority
+        onClick={() => confetti()}
+      />
+
+      <span>
+        <h2>
+          {title}
+        </h2>
+        <p>{description}</p>
+      </span>
+
+    </a >
   )
 }
 
 export default function Home() {
+  const [displayProfile, setDisplayProfile] = useState(true);
+
+  function toggleProfile() {
+    setDisplayProfile(!displayProfile)
+  }
   return (
     <span className={roboto.className}>
       <main className={styles.main}>
-
-        <div className={styles.center}>
-          <div className={styles.description}>
-            <Image
-              src="./images/profile.jpg"
-              alt="Profile picture"
-              className={styles.profilePicture}
-              width={0}
-              height={0}
-              sizes="10vw"
-              style={{ width: '15rem', height: 'auto' }} // optional
-              priority
-            />
-          </div>
-          <h2 className={styles.profileName}>Juan Ignacio Sánchez Sampayo</h2>
-          <div className={styles.description}>
-
-            <p>I am a senior backend engineer, based in Montevideo, Uruguay, and have 9+ years
-              of experience working with Python/Django and similar technologies. I build systems,
-              robust RESTful APIs and web applications for many clients, focusing on the product,
-              finding solutions with autonomy, well balanced regarding delivery time and quality.
-              My strongest suit is in backend technologies but I`&apos;`m currently learning React/Next.js, this website is powered by such framework.</p>
-          </div>
-        </div>
-
+        <Profile displayProfile={displayProfile} toggleProfile={toggleProfile} />
 
         <div className={styles.grid}>
-          <PageCard title="Contact Information" description="If you would like to do business, please reach out!" />
-          <PageCard title="Work Experience" description="If you would like to do business, please reach out!" />
-          <PageCard title="Education" description="If you would like to do business, please reach out!" />
-          <PageCard title="Personal Projects" description="If you would like to do business, please reach out!" />
+          <PageCard initialTitle="Projects" description="If you would like to do business, please reach out!" icon="science" />
+          <PageCard initialTitle="Skills" description="If you would like to do business, please reach out!" icon="flask" />
+          <PageCard initialTitle="About me" description="If you would like to do business, please reach out!" icon="about" />
+
         </div>
       </main>
     </span>
