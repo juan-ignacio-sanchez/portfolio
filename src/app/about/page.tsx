196110c0c1824cba2a5aa034b410fb7dd4d7eb51
Juan Ignacio Sánchez Sampayo <juan@192.168.1.14>
2023-10-15 18:58:10 -0300

    add tile background image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,9 @@ import styles from "./page.module.css";
 export default function About() {
   return (
     <main>
-      <div className={styles.main}>About</div>
+      <div className={styles.main}>
+        <h1>All roads lead to Rome</h1>
+      </div>
     </main>
   );
 }
