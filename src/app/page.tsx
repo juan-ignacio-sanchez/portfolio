3f9fa72d9f7fc3c2fa0b22147f99a3d1d6acbbad
Juan Ignacio Sánchez Sampayo <juan@192.168.1.7>
2023-11-22 23:20:08 -0300

    use built-in fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
